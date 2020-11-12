16dbcae715128946de3c9fa1906fb14b81d28284
Sigbjørn Skolem Ledaal <sigbjl18@uia.no>
2020-11-12 21:06:04 +0100

    First working build

--- src/components/Chart.svelte
+++ src/components/Chart.svelte
@@ -2,47 +2,72 @@
     import FusionCharts from 'fusioncharts';
     import Timeseries from 'fusioncharts/fusioncharts.timeseries';
     import SvelteFC, { fcRoot } from 'svelte-fusioncharts';
-    import yolo from './data';
+    import { onMount } from 'svelte';
+    import { bigdata } from './data';
 
     fcRoot(FusionCharts, Timeseries);
 
-    let schema1 = [{
-    "name": "Time",
-    "type": "date",
-    "format": "%-m/%-d/%Y"
-}, {
-    "name": "Sales",
-    "type": "number"
-}]
+    const getTemp = async () => {
+        let res = await fetch('https://api.darkfiber.no/temp');
+        let datan = await res.json();
 
-    const getChartConfig = ([data, schema]) => {
-        const fusionDataStore = new FusionCharts.DataStore(),
-        fusionTable = fusionDataStore.createDataTable(data, schema);
+        let ee = [];
+        datan.forEach((item) => {
+            ee.push([item.timestamp, item.value])
+        });
+
+        return ee;
+    }
+
+    let temps;
+
+    onMount(() => {
+        temps = getTemp();
+    });
+
+    //setInterval(updateTemp, 1000*60);
+
+    let yolo = [
+    {
+        "name": "Time",
+        "type": "date",
+        "format": "%s"
+    },
+    {
+      "name": "Temp",
+      "type": "number"
+    }
+    ]
+
+    let gg = []
+    bigdata.forEach((item) => {
+        gg.push([item.timestamp, item.value])
+    })
+
+
+    const getChartConfig = (data, schema) => {
+        const fusionDataStore = new FusionCharts.DataStore()
+        const fusionTable = fusionDataStore.createDataTable(data, schema);
 
         return {
         type: 'timeseries',
         width: '100%',
-        height: 450,
+        height: 550,
         renderAt: 'chart-container',
+        dataFormat: 'json',
         dataSource: {
             data: fusionTable,
             caption: {
-            text: 'Sales Analysis'
-            },
-            subcaption: {
-            text: 'Grocery'
+                text: 'Temperature readings from PI'
             },
             yAxis: [
-            {
-                plot: {
-                value: 'Grocery Sales Value',
-                type: 'line'
-                },
-                format: {
-                prefix: '$'
-                },
-                title: 'Sale Value'
-            }
+                {
+                    plot: {
+                        value: 'Temp',
+                        type: 'line'
+                    },
+                    title: 'Temp'
+                }
             ]
         }
         };
@@ -51,5 +76,5 @@
 </script>
 
 <div id="chart-container">
-    <SvelteFC {...getChartConfig([yolo, schema1])}/>
+    <SvelteFC {...getChartConfig(gg, yolo)}/>
 </div>
